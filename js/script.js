6b8fd2aa6b4d40c484e097f3273065291a218ca8
cosubhasmos <cosubhasmos@gmail.com>
2026-01-15 11:35:12 +0545

    First add

---
+++ js/script.js
@@ -0,0 +1,212 @@
+
+
+(function ($) {
+  'use strict';
+
+  /* ========================================================================= */
+  /*	Page Preloader
+  /* ========================================================================= */
+  $(window).on('load', function () {
+    $('#preloader').fadeOut('slow', function () {
+      $(this).remove();
+    });
+  });
+
+
+  // navbarDropdown
+	if ($(window).width() < 992) {
+		$('#navigation .dropdown-toggle').on('click', function () {
+			$(this).siblings('.dropdown-menu').animate({
+				height: 'toggle'
+			}, 300);
+		});
+  }
+  
+  //Hero Slider
+  $('.hero-slider').slick({
+    autoplay: true,
+    infinite: true,
+    arrows: true,
+    prevArrow: '<button type=\'button\' class=\'prevArrow\'></button>',
+    nextArrow: '<button type=\'button\' class=\'nextArrow\'></button>',
+    dots: false,
+    autoplaySpeed: 7000,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+  $('.hero-slider').slickAnimation();
+
+  /* ========================================================================= */
+  /*	Portfolio Filtering Hook
+  /* =========================================================================  */
+  // filter
+  setTimeout(function(){
+    var containerEl = document.querySelector('.filtr-container');
+    var filterizd;
+    if (containerEl) {
+      filterizd = $('.filtr-container').filterizr({});
+    }
+  }, 500);
+
+  /* ========================================================================= */
+  /*	Testimonial Carousel
+  /* =========================================================================  */
+  //Init the slider
+  $('.testimonial-slider').slick({
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+
+
+  /* ========================================================================= */
+  /*	Clients Slider Carousel
+  /* =========================================================================  */
+  //Init the slider
+  $('.clients-logo-slider').slick({
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false
+      }
+    }
+    ]
+  });
+
+  /* ========================================================================= */
+  /*	Company Slider Carousel
+  /* =========================================================================  */
+  $('.company-gallery').slick({
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 667,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false
+      }
+    }
+    ]
+  });
+
+  /* ========================================================================= */
+  /*	On scroll fade/bounce effect
+  /* ========================================================================= */
+  var scroll = new SmoothScroll('a[href*="#"]');
+
+  // -----------------------------
+  //  Count Up
+  // -----------------------------
+  function counter() {
+    var oTop;
+    if ($('.counter').length !== 0) {
+      oTop = $('.counter').offset().top - window.innerHeight;
+    }
+    if ($(window).scrollTop() > oTop) {
+      $('.counter').each(function () {
+        var $this = $(this),
+          countTo = $this.attr('data-count');
+        $({
+          countNum: $this.text()
+        }).animate({
+          countNum: countTo
+        }, {
+          duration: 1000,
+          easing: 'swing',
+          step: function () {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function () {
+            $this.text(this.countNum);
+          }
+        });
+      });
+    }
+  }
+  // -----------------------------
+  //  On Scroll
+  // -----------------------------
+  $(window).scroll(function () {
+    counter();
+
+    var scroll = $(window).scrollTop();
+    if (scroll > 50) {
+      $('.navigation').addClass('sticky-header');
+    } else {
+      $('.navigation').removeClass('sticky-header');
+    }
+  });
+
+// Set the date we're counting down to
+var countDownDate = new Date("Apr 12, 2023 15:00:00").getTime();
+
+// Update the countdown every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Calculate the distance between now and the countdown date
+  var distance = countDownDate - now;
+
+  // Calculate days, hours, minutes, and seconds left
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the countdown timer
+  document.querySelector(".days").innerHTML = days;
+  document.querySelector(".hours").innerHTML = hours;
+  document.querySelector(".minutes").innerHTML = minutes;
+  document.querySelector(".seconds").innerHTML = seconds;
+
+  // If the countdown is finished, display a message
+  if (distance < 0) {
+    clearInterval(x);
+    document.querySelector(".timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
+})(jQuery);
